22bf6c6a978263fa6753fdd2b5c8de65e568bdab
LeoAnders <leonardoanders15@gmail.com>
2023-08-21 19:45:25 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Home() {
   const [count, setCount] = useState(0);
@@ -9,7 +9,10 @@ function Home() {
 
   return (
     <div>
-      <h1>"O sucesso é a soma de pequenos esforços repetidos dia após dia." - Robert Collier</h1>
+      <h1>
+        "O sucesso é a soma de pequenos esforços repetidos dia após dia." -
+        Robert Collier
+      </h1>
       <p>Count: {count}</p>
       <button onClick={handleClick}>Like</button>
     </div>
