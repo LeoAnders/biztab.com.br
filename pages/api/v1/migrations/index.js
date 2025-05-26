806dc3a43b50815f254ddc46aaf696be400dea17
LeoAnders <leonardoanders15@gmail.com>
2025-05-26 20:22:55 -0300

    refactor: extract `migration logic` to dedicated `migrator model`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,57 +1,25 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import controller from "infra/controller.js";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
 export default router.handler(controller.errorHandlers);
 
-router.get(getMigrationsHandle).post(postMigrationsHandle);
+router.get(getMigrationsHandle);
+router.post(postMigrationsHandle);
 
 async function getMigrationsHandle(request, response) {
-  const { dbClient, defaultMigrationOptions } = await initializeMigrations({});
-
-  const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-  await dbClient?.end();
+  const pendingMigrations = await migrator.listPendingMigrations();
   return response.status(200).json(pendingMigrations);
 }
 
 async function postMigrationsHandle(request, response) {
-  const { dbClient, defaultMigrationOptions } = await initializeMigrations({
-    dryRun: false,
-  });
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-  const migratedMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dryRun: false,
-  });
-
-  await dbClient.end();
   if (migratedMigrations.length > 0) {
     return response.status(201).json(migratedMigrations);
   }
 
   return response.status(200).json(migratedMigrations);
-}
-async function initializeMigrations({ dryRun = true }) {
-  let dbClient;
-
-  try {
-    dbClient = await database.getNewClient();
-
-    const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: dryRun,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
-
-    return { dbClient, defaultMigrationOptions };
-  } catch (error) {
-    await dbClient?.end();
-  }
 }
