ff20a50fb9867fe60c04c87299d3fe97c0a354bc
LeoAnders <leonardoanders15@gmail.com>
2025-07-06 20:15:47 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -79,3 +79,24 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Could not find the resource in the system"),
+      {
+        cause,
+      };
+    this.name = "NotFoundError";
+    this.action = action || "Check if the query parameters are correct";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
