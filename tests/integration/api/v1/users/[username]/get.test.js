dcdacbb41b3d9351bbbbe8a4d6cc902c22f0de38
LeoAnders <leonardoanders15@gmail.com>
2025-09-06 16:38:04 -0300

    feat: hash `password` in `user.create`

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -36,7 +36,7 @@ describe("GET /api/v1/users/[username]", () => {
         id: response2Body.id,
         username: "SameCase",
         email: "same.case@example.com",
-        password: "ayt27%4",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
@@ -73,7 +73,7 @@ describe("GET /api/v1/users/[username]", () => {
         id: response2Body.id,
         username: "DifferentCase",
         email: "different.case@example.com",
-        password: "ayt27%4",
+        password: response2Body.password,
         created_at: response2Body.created_at,
         updated_at: response2Body.updated_at,
       });
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator";
+import user from "models/user";
+import password from "models/password";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -30,7 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "leonardoanders",
         email: "leonardoanders@example.com",
-        password: "ayt27%4",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -38,6 +40,18 @@ describe("POST /api/v1/users", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const userInDatabase = await user.findOneByUsername("leonardoanders");
+      const correctPasswordMatch = await password.compare(
+        "ayt27%4",
+        userInDatabase.password,
+      );
+      const incorrectPasswordMatch = await password.compare(
+        "incorretPassword",
+        userInDatabase.password,
+      );
+      expect(correctPasswordMatch).toBe(true);
+      expect(incorrectPasswordMatch).toBe(false);
     });
 
     test("With duplicated 'email'", async () => {
